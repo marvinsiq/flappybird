8dd2504d207b064cce85ef34c9de81f879045fda
Marcus Siqueira <marvinsiq@gmail.com>
2020-02-05 23:14:03 -0300

    Adicionando cursor na tela

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private SpriteRenderer imagem;
+
+    private void Awake()
+    {
+        this.imagem = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.Desaparecer();
+        }
+    }
+
+    private void Desaparecer()
+    {
+        this.imagem.enabled = false; 
+    }
+}
